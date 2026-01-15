c8cd9520b0f015c560a8e372b71477b071234605
Lokitha-S <lokithasoundar@gmail.com>
2026-01-15 19:38:41 +0530

    first commit

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+#Day 1
+name=input('Enter ur name:')#takes input from user by "input keyword"
+internship_role=input('Enter ur role:')
+date=input('Enter ur join date:')
+print(f"{name} is a {internship_role},joined on {date}")#use f before" to place the variables 
